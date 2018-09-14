d7759d048be4fb81758a744cda290a2f54b2dcd4
gpsingh9999 <gajender.singh@myoperator.co>
2018-09-14 12:51:17 +0530

    added file download logic and added commenting

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 import yaml
 import boto3
 import io
+import requests
 
 
 class Solution:
@@ -15,23 +16,34 @@ class Solution:
         self.config = config
         self.response = {'error': True, 'response': ''}
 
-
-    def excelToJson(self, filename, sheet_name):
-        self.logger.info(f'reading excel file | {filename}')
-        data = pd.read_excel(filename, sheet_name=sheet_name).to_json(orient='records')
-        return self.uploadJson(data)
+    # main function which will handle all the processing
+    def excelToJson(self, sheet_name):
+        # download the excel to the tile path specified in config file
+        if self.downloadExcel():
+            self.logger.info(f"reading excel file | {self.config['excel_filepath']}")
+            # read the excel sheet and convert the sheet to json
+            data = pd.read_excel(self.config['excel_filepath'], sheet_name=sheet_name).to_json(orient='records')
+            # upload the json data to S3 object
+            return self.uploadJson(data)
+        else:
+            self.response['response'] = f"not able to write excel file from the URL"
+            return self.response
 
     def uploadJson(self, data):
         self.logger.debug('calling AWS S3 service')
+        # make client of aws S3
         try:
             s3 = boto3.client('s3',
                               aws_access_key_id=self.config['aws_access_key'],
                               aws_secret_access_key=self.config['aws_secret_access_key'])
+            # convert the string utf-8 to bytes for uploading to S3 bucket
             data = io.BytesIO(bytearray(data, encoding='utf-8'))
+
+            # upload the data to s3 bucket
             s3.put_object(Bucket=self.config['bucket_name'], Key=self.config['json_object_keyname'], Body=data,
                           ACL='public-read')
             self.response[
-                'response'] = f"https://s3-us-west-2.amazonaws.com/{self.config['bucket_name']}/{self.config['json_object_keyname']}"
+                'response'] = f"https://s3-{self.config['aws_region']}.amazonaws.com/{self.config['bucket_name']}/{self.config['json_object_keyname']}"
             self.response['error'] = False
         except Exception as e:
             msg = f"write json object in S3 | {e}"
@@ -39,6 +51,17 @@ class Solution:
             self.response['response'] = msg
         return self.response
 
+    def downloadExcel(self):
+        try:
+            # download the excel file from given URL
+            r = requests.get(self.config['excel_url'], allow_redirects=True)
+
+            # write the file in the temp directory
+            open(self.config['excel_filepath'], 'wb').write(r.content)
+            return True
+        except Exception as e:
+            self.logger.info(f"not able to download excel data to xls file | {e}")
+            return False
 
 def loadConfig(filename, logger):
     try:
@@ -63,14 +86,16 @@ def loadLogger():
 def lambda_handler(event, context):
     logger = loadLogger()
     logger.info('logger loaded')
+    # load config data from the config.yaml file
     config = loadConfig('config.yaml', logger)
     if not config:
         return {'response': 'not able to extract the credentials', 'error': True}
 
     logger.info('config loaded')
     sol = Solution(config, logger)
-    return sol.excelToJson('ISO10383_MIC.xls', 'MICs List by CC')
+    return sol.excelToJson('MICs List by CC')
 
 
 if __name__ == '__main__':
+    # for debugging purpose
     print(lambda_handler(1, 2))
